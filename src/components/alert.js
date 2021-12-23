193a97e054d7209282c8ce2e9e749ca1ec1edae1
Dan Costa <dancosta.id@gmail.com>
2021-12-23 10:16:07 -0300

    I've added the notification when page load

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,26 +1,49 @@
-import React, { alert } from "react";
+import React, { alert, useState, useEffect } from "react";
+import ReactNotification from 'react-notifications-component';
+import { store } from 'react-notifications-component';
+import { Animated } from "react-animated-css"
+import 'react-notifications-component/dist/theme.css'
 
 
-const AddAlert = () => {
+function AddAlert() {
     return (
-        <div className='closeBtn'>
-
-            <alert className='alertBox'>Search a Movie on the box above!</alert>
-
-            <svg xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="red"
-                className="bi bi-x-square-fill close"
-                viewBox="0 0 16 16">
-                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
-            </svg>
-
-
+        <div className='container notiBox'>
+            <ReactNotification />
+            <Home />
         </div>
-
     )
-};
+}
+
+
+function Home() {
+
+    useEffect(() => {
+        setTimeout(() => {
+            store.addNotification({
+                title: 'If nothing is showing...',
+                message: `Don't forget to add a movie title to the search bar!`,
+                type: 'danger',
+                container: 'top-center',
+                insert: 'top',
+                animationIn: ['animate__animated', 'animate__fadeIn'],
+                animationOut: ['animate__animated', 'animate__slideInDown'],
+                width: 800,
+                dismiss: {
+                    duration: 5000,
+                    showIcon: true,
+                    onScreen: true
+                }
+            })
+        }, [3500])
+
+    }, [])
+
+    return (
+        null
+    )
+}
+
+
 
 export default AddAlert;
 
